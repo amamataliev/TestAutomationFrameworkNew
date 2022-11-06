0b818734f2cb4a3f17e2bfe45e60a9eb9dfb7958
Aibek <aibek.mamataliev@gmail.com>
2022-11-06 15:34:15 +0600

    Done!

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -1,26 +1,37 @@
-//package steps;
-//
-//import com.digital_nomads.driverFactory.Driver;
-//import cucumber.api.Scenario;
-//
-//import cucumber.api.java.After;
-//import org.openqa.selenium.OutputType;
-//import org.openqa.selenium.TakesScreenshot;
-//
-//public class Hooks {
-//
-//
-//    @After
-//    public void tearDown(Scenario scenario){
-//        try{
-//            if(scenario.isFailed()){
-//                final byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
-//                scenario.attach(screenshot, "image/png", "screenshot");
-//            }
-//        }catch (Exception e){
-//            e.printStackTrace();
-//
-//        }
-//        Driver.closeDriver();
-//    }
-//}
+package steps;
+
+
+import com.digital_nomads.driverFactory.Driver;
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+
+public class Hooks {
+
+
+    @Before
+    public void setUp() {
+        System.out.println("Before hook started");
+    }
+
+
+    @After
+    public void tearDown(Scenario scenario) {
+
+        try {
+            if (scenario.isFailed()) {
+                final byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+                scenario.embed(screenshot, "image/png");
+            }
+        } catch (Exception e) {
+            System.out.println("The error happened while cleaning up after the test: " +
+                    e.getMessage());
+        }
+
+        Driver.closeDriver();
+        System.out.println("After hook driver is closed");
+    }
+}
